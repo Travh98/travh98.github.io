1cb9c8c3ccf9172f9e95ca5add53a41e47b98c44
travh98 <travishunter33@gmail.com>
2022-12-28 07:25:29 -0500

    added svelte

---
+++ src/routes/ProjectCard.svelte
@@ -0,0 +1,24 @@
+<script>
+    let title = 'Project Name';
+    let date = 'today';
+    let taglist = ['HTML', 'CSS', 'Typescript', 'Svelte'];
+</script>
+
+<style>
+    .card {
+        border-radius: 1rem;
+        background-color: rgb(25, 25, 25);
+    }
+    #title {
+        float: left;
+    }
+    #date {
+        float: right;
+    }
+</style>
+
+<div class='card'>
+    <h2 id='title'>{title}</h2>
+    <h2 id='date'>{date}</h2>
+</div>
+
